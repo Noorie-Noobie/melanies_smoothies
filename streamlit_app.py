967b67f98de8d17aab1b5987f537ef9fc586c59c
Noorie-Noobie <144753949+Noorie-Noobie@users.noreply.github.com>
2024-03-20 17:38:48 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("🥤Customize Your Smoothie!🥤")
@@ -23,28 +24,27 @@ ingredients_list = st.multiselect('Choose up to 5 ingredients:'
 )
 
 if ingredients_list:
-    ingredients_string = ''
+        ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
-            ingredients_string += fruit_chosen + ' '
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-            fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        for fruit_chosen in ingredients_list:
+                ingredients_string += fruit_chosen + ' '
+                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+                fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string+"""','"""+name_on_order+"""')"""
     #st.write(my_insert_stmt)
     #st.stop()
  
 
-    time_to_insert = st.button('Submit Order')
+        time_to_insert = st.button('Submit Order')
     
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+        if time_to_insert:
+                session.sql(my_insert_stmt).collect()
         
-        st.success('Your Smoothie is ordered, {}!'.format(name_on_order), icon="✅")
+                st.success('Your Smoothie is ordered, {}!'.format(name_on_order), icon="✅")
 
 
 
-import requests
